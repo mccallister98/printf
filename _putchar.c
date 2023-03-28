3bdb4c2e15cf1bc1fc0be393cbad5a361f36446a
mccallister98 <machatiwandi@outlook.com>
2023-03-29 00:37:23 +0200

    Putchar file

---
+++ _putchar.c
@@ -0,0 +1,15 @@
+able File  13 lines (12 sloc)  246 Bytes
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
